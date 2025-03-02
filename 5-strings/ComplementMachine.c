993142c6fff239a9938e7bc6d48c39f557c41a1e
NafiurSiddiqui <saejim91@gmail.com>
2025-03-02 21:56:02 +0600

    nothing crazy

--- 5-strings/ComplementMachine.c
+++ 5-strings/ComplementMachine.c
@@ -3,11 +3,11 @@
 //a string is a sequences of characters
 int main(){
 
-    char name[31];
     //you need to define array of characters to make a string
     //you need to set the limit of characters
     //ALWAYS add one extra character limit. This is to define where the string ends. This is known as "Null Terminator"
-    printf("Your name: ");
+    char name[31];
+    printf("Your name boi: ");
     scanf("%s", name); //In case of arrays, you DONT put reference operator
     printf("Hello,%s!\nYou look gockagious today!\n", name); 
     return 0;
